25fa670a12211d7f24ca63d6d01b8fdbaf4a80b6
Artem Chernyshov <silence_made@mail.ru>
2023-05-31 21:01:39 +0300

    delete OUT + new sort

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-        numbers.stream()
-                .filter(x -> x > 0)
-                .filter(x -> x % 2 == 0)
-                .sorted(Comparator.naturalOrder())
-                .forEach(System.out::println);
+        List<Integer> sortedNumbers = new ArrayList<>();
+        for (int number: numbers) {
+           if (number > 0 && number % 2 == 0){
+               sortedNumbers.add(number);
+           }
+        }
+        sortedNumbers.sort(Integer::compareTo);
+        System.out.println(sortedNumbers);
     }
 }
